89d939082bc0ab029f21988d1bd78ca652f9154f
davign <64040506+DavigNascimento@users.noreply.github.com>
2025-11-19 11:56:39 -0300

    add: sistema de gols nas Disputas

--- src/main/java/com/fut7/models/disputas/Disputa.java
+++ src/main/java/com/fut7/models/disputas/Disputa.java
@@ -1,5 +1,8 @@
 package com.fut7.models.disputas;
 
+import java.util.HashMap;
+
+import com.fut7.models.Jogador;
 import com.fut7.models.Time;
 import com.fut7.models.TAD.Lista;
 
@@ -12,4 +15,9 @@ public class Disputa {
     
     private final Lista<Time> times;
     private TiposDisputa fase;
+    
+    // Resultado
+    private HashMap<Time, Integer> golsPorTime;
+    private HashMap<Jogador, Integer> golsPorJogador;
+    private HashMap<Jogador, Integer> assistenciasPorJogador;
 }
